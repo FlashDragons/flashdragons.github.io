71837bdd04086e6b44ad4bd9616574f03c88f29f
FlashDragons <spring_horizon@163.com>
2026-01-26 12:09:47 +0800

    添加klpbbs的3个存档页面。

---
+++ bbs/klp/klpbbs-original_files/home.php
@@ -0,0 +1,31 @@
+var _____WB$wombat$assign$function_____=function(name){return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name))||self[name];};if(!self.__WB_pmw){self.__WB_pmw=function(obj){this.__WB_source=obj;return this;}}{
+let window = _____WB$wombat$assign$function_____("window");
+let self = _____WB$wombat$assign$function_____("self");
+let document = _____WB$wombat$assign$function_____("document");
+let location = _____WB$wombat$assign$function_____("location");
+let top = _____WB$wombat$assign$function_____("top");
+let parent = _____WB$wombat$assign$function_____("parent");
+let frames = _____WB$wombat$assign$function_____("frames");
+let opens = _____WB$wombat$assign$function_____("opens");
+
+}
+
+/*
+     FILE ARCHIVED ON 03:24:02 Jan 26, 2026 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 03:35:12 Jan 26, 2026.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.466
+  exclusion.robots: 0.022
+  exclusion.robots.policy: 0.013
+  esindex: 0.006
+  cdx.remote: 9.174
+  LoadShardBlock: 147.489 (3)
+  PetaboxLoader3.datanode: 78.698 (3)
+  load_resource: 3.538
+*/
